228328d69319ccbb3a9b3ce6fc7e3e8970ecaa6d
superstar54 <xingwang1991@gmail.com>
2020-03-18 20:07:49 +0100

    fix readme

--- example/fetch_sciencedirect.py
+++ example/fetch_sciencedirect.py
@@ -3,11 +3,7 @@ url = 'http://www.sciencedirect.com/science/article/pii/S1751616116301138'
 keyword = 'CALPHAD'
 
 liter = Pyliterature(url, keyword)
-# load text from url html and parse the sentences including keyword
 liter.parser()
-
-print('=================article text==================================')
-# print(liter.text)
 
 print('=================key sentences==================================')
 for keysent in liter.keysents:
